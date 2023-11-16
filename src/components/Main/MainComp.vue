f5fee06cccc60a93738d0c6d7e4327da2972f8c1
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-11-16 16:11:08 +0100

    flag

--- src/components/Main/MainComp.vue
+++ src/components/Main/MainComp.vue
@@ -7,7 +7,9 @@
                     <h4>{{ movie.title }}</h4>
                     <ul>
                         <li>{{ movie.original_title }}</li>
-                        <li>{{ movie.original_language }}</li>
+                        <li>
+                            <img :src="getFlag" :alt="movie.original_language" class="myfleg">
+                        </li>
                         <li>{{ movie.vote_average }}</li>
                     </ul>
                 </li>
@@ -25,10 +27,25 @@ export default {
     name: 'MainComp',
     data() {
         return {
-            store
+            store,
+            flags: ['de', 'fr', 'it', 'jp', 'kr', 'es', 'uk'],
+            imgPath: 'https://image.tmdb.org/t/p/original/'
+        }
+    },
+    computed: {
+        getFlag() {
+            let flag = `/public/images/${this.original_language}.png`
+            if (!this.flags.includes(this.original_language)) {
+                flag = '/public/images/fake.png'
+            }
+            return flag
         }
     }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.myfleg {
+    width: 50px;
+}
+</style>
